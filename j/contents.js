44bb503f83e1915369a06bd117c21fb613e85c58
minocolor <minocolor@gmail.com>
2012-03-10 19:55:38 +0900

    キービジュアルスライド

--- j/contents.js
+++ j/contents.js
@@ -1,3 +1,35 @@
+//キービジュアルスライド
+function slideSwitch() {
+    var $active = $('.keyVisualContainer img.active');
+
+    if ( $active.length == 0 ) $active = $('.keyVisualContainer img:last');
+
+    // use this to pull the images in the order they appear in the markup
+    var $next =  $active.next().length ? $active.next()
+        : $('.keyVisualContainer img:first');
+
+    // uncomment the 3 lines below to pull the images in random order
+    
+    // var $sibs  = $active.siblings();
+    // var rndNum = Math.floor(Math.random() * $sibs.length );
+    // var $next  = $( $sibs[ rndNum ] );
+
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
+
+
+
 //contentsContainerナビ マウスオーバー
 $(function() {
     $('.localNav li')
